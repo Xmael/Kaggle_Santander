c24555c332f4b47038a7efc593b704b4f8f72ee3
Xmael <ismagm@gmail.com>
2016-05-07 14:05:36 +0200

    Stack 0.1.4
    Corregido problema con los folds

--- stack.R
+++ stack.R
@@ -5,7 +5,6 @@ library(e1071)
 library(xgboost)
 library(AUC)
 library(stringr)
-library(strindist)
 library(h2o)
 library(foreign) #Solo para cargar el conjunto de datos de prueba en formato arff
 setwd("~/Kaggle/Santander")
@@ -33,18 +32,18 @@ inicializa=function(){
 
 inicializa()
 
-añade=function(stacks,learners_train,learners_test,modelos,algoritmo,parametros){
+añade=function(nfolds,folds,stacks,learners_train,learners_test,modelos,algoritmo,parametros){
   aux=rep(NA,nrow(train))
-  for(i in 1:5){
+  for(i in 1:nfolds){
     aux[folds[[i]]]=stacks[[i]][[1]]
   }
   cat("Metrica\n")
-  metrica=mean(sapply(1:5,function(i)stacks[[i]][[3]]))
+  metrica=mean(sapply(1:nfolds,function(i)stacks[[i]][[3]]))
   print(metrica)
   lee=readline(prompt = "Añadir al stack? [Y]/[N]: ")
   if(str_to_upper(lee)=="Y" | lee==""){
     learners_train=cbind(learners_train,aux)
-    learners_test=cbind(learners_test,apply(sapply(1:5,function(i)stacks[[i]][[2]]),1,mean))
+    learners_test=cbind(learners_test,apply(sapply(1:nfolds,function(i)stacks[[i]][[2]]),1,mean))
     print(head(learners_test))
     modelos=rbind(modelos,cbind(algoritmo=algoritmo,parametros=parametros,metrica=metrica))
     colnames(learners_train)[ncol(learners_train)]=algoritmo
@@ -58,8 +57,8 @@ añade=function(stacks,learners_train,learners_test,modelos,algoritmo,parametros
 }
 
 ####KNN
-stack_knn=function(train,test,folds,k){
-  stacks=lapply(1:5,function(i,train,test,folds,k){
+stack_knn=function(nfolds,train,test,folds,k){
+  stacks=lapply(1:nfolds,function(i,train,test,folds,k){
     print(i)
     train_stack=train[-folds[[i]],]
     test_stack=train[folds[[i]],]
@@ -72,12 +71,12 @@ stack_knn=function(train,test,folds,k){
   },train,test,folds,k)
   algoritmo="knn"
   parametros=paste("k=",k,sep='')
-  añade(stacks,learners_train,learners_test,modelos,algoritmo,parametros)
+  añade(nfolds,folds,stacks,learners_train,learners_test,modelos,algoritmo,parametros)
 }
 
 #RANDOM FOREST
-stack_rf=function(train,test,folds,ntree){
-  stacks=lapply(1:5,function(i,train,test,folds,ntree){
+stack_rf=function(nfolds,train,test,folds,ntree){
+  stacks=lapply(1:nfolds,function(i,train,test,folds,ntree){
     print(i)
     train_stack=train[-folds[[i]],]
     test_stack=train[folds[[i]],]
@@ -89,12 +88,12 @@ stack_rf=function(train,test,folds,ntree){
   },train,test,folds,ntree)
   algoritmo="rf"
   parametros=paste("ntree=",ntree,sep='')
-  añade(stacks,learners_train,learners_test,modelos,algoritmo,parametros)
+  añade(nfolds,folds,stacks,learners_train,learners_test,modelos,algoritmo,parametros)
 }
 
 #SVM
-stack_svm=function(train,test,folds,kernel){
-  stacks=lapply(1:5,function(i,train,folds,kernel){
+stack_svm=function(nfolds,train,test,folds,kernel){
+  stacks=lapply(1:nfolds,function(i,train,folds,kernel){
     print(i)
     train_stack=train[-folds[[i]],]
     test_stack=train[folds[[i]],]
@@ -106,7 +105,7 @@ stack_svm=function(train,test,folds,kernel){
   },train,folds,kernel)
   algoritmo="svm"
   parametros=paste("kernel=",kernel,sep='')
-  añade(stacks,learners_train,learners_test,modelos,algoritmo,parametros)
+  añade(nfolds,folds,stacks,learners_train,learners_test,modelos,algoritmo,parametros)
 }
 
 params_xgboost=list(nthred=1,
@@ -120,8 +119,8 @@ params_xgboost=list(nthred=1,
                     maximize = T,
                     eval_metric = "auc")
 
-stack_xgboost=function(train,test,folds,eta,nrounds){
-  stacks=lapply(1:5,function(i,train,test,folds,eta,nrounds){
+stack_xgboost=function(nfolds,train,test,folds,eta,nrounds){
+  stacks=lapply(1:nfolds,function(i,train,test,folds,eta,nrounds){
     print(i)
     train_stack=xgb.DMatrix(data=data.matrix(train[-folds[[i]],-ncol(train)]),label=as.numeric(levels(train$Class[-folds[[i]]]))[train$Class[-folds[[i]]]])
     test_stack=xgb.DMatrix(data=data.matrix(train[folds[[i]],-ncol(train)]),label=as.numeric(levels(train$Class[folds[[i]]]))[train$Class[folds[[i]]]])
@@ -133,14 +132,14 @@ stack_xgboost=function(train,test,folds,eta,nrounds){
   },train,test,folds,eta,nrounds)
   algoritmo="xgboost"
   parametros=paste("eta=",eta,sep='')
-  añade(stacks,learners_train,learners_test,modelos,algoritmo,parametros)
+  añade(nfolds,folds,stacks,learners_train,learners_test,modelos,algoritmo,parametros)
 } 
 
 h2o.init(ip = "localhost",port = 54321,nthreads = 2,max_mem_size = "4G")
 
 #H2O Random Forests
-stack_h2orf=function(train,test,folds,ntrees){
-  stacks=lapply(1:5,function(i,train,test,folds,ntrees){
+stack_h2orf=function(nfolds,train,test,folds,ntrees){
+  stacks=lapply(1:nfolds,function(i,train,test,folds,ntrees){
     print(i)
     train_stack=as.h2o(train[-folds[[i]],],destination_frame = "train_stack")
     test_stack=as.h2o(train[folds[[i]],],destination_frame = "test_stack")
@@ -163,12 +162,12 @@ stack_h2orf=function(train,test,folds,ntrees){
   },train,test,folds,ntrees)
   algoritmo="h2orf"
   parametros=paste("ntrees=",ntrees,sep='')
-  añade(stacks,learners_train,learners_test,modelos,algoritmo,parametros)
+  añade(nfolds,folds,stacks,learners_train,learners_test,modelos,algoritmo,parametros)
 } 
 
 #H2O deep learning
-stack_h2odeep=function(train,test,folds,activation,initial_weight_distribution,hidden,hidden_dropout_ratios,epochs,rate,l2){
-  stacks=lapply(1:5,function(i,train,test,folds,activation,initial_weight_distribution,hidden,hidden_dropout_ratios,epochs,rate,l2){
+stack_h2odeep=function(nfolds,train,test,folds,activation,initial_weight_distribution,hidden,hidden_dropout_ratios,epochs,rate,l2){
+  stacks=lapply(1:nfolds,function(i,train,test,folds,activation,initial_weight_distribution,hidden,hidden_dropout_ratios,epochs,rate,l2){
     print(i)
     train_stack=as.h2o(train[-folds[[i]],],destination_frame = "train_stack")
     test_stack=as.h2o(train[folds[[i]],],destination_frame = "test_stack")
@@ -203,15 +202,17 @@ stack_h2odeep=function(train,test,folds,activation,initial_weight_distribution,h
   algoritmo="h2odeep"
   parametros=paste("activation=",activation," initial_weight_distribution",initial_weight_distribution," hidden",hidden," hidden_dropout_ratios",hidden_dropout_ratios,
                    " epochs",epochs," rate",rate," l2",l2,sep='')
-  añade(stacks,learners_train,learners_test,modelos,algoritmo,parametros)
+  añade(nfolds,folds,stacks,learners_train,learners_test,modelos,algoritmo,parametros)
 } 
 
-stack_knn(train=train,test=test,folds=folds,k=13)
-stack_rf(train=train,test=test,folds=folds,ntree=50)
-stack_svm(train=train,test=test,folds=folds,kernel="radial")
-stack_xgboost(train=train,test=test,folds=folds,eta=0.5,nrounds=20)
-stack_h2orf(train=train,test=test,folds=folds,ntrees=100)
-stack_h2odeep(train=train,test=test,folds=folds,activation=,initial_weight_distribution=,
+######Llamadas
+
+stack_knn(nfolds=nfolds,train=train,test=test,folds=folds,k=3)
+stack_rf(nfolds=nfolds,train=train,test=test,folds=folds,ntree=50)
+stack_svm(nfolds=nfolds,train=train,test=test,folds=folds,kernel="radial")
+stack_xgboost(nfolds=nfolds,train=train,test=test,folds=folds,eta=0.5,nrounds=20)
+stack_h2orf(nfolds=nfolds,train=train,test=test,folds=folds,ntrees=100)
+stack_h2odeep(nfolds=nfolds,train=train,test=test,folds=folds,activation=,initial_weight_distribution=,
               hidden=,hidden_dropout_ratios=,epochs=,rate=,l2=)
 
 learners_train_bak=learners_train
@@ -243,35 +244,33 @@ learners_test_1st=learners_test
 modelos_1st=modelos
 
 pfolds=createFolds(y=learners_train_1st$Class,k=nfolds-1,list=T)
-learners_train=NULL
-learners_test=NULL
-modelos=NULL
+inicializa()
 
-stack_knn(train=learners_train_1st,test=learners_test_1st,folds=pfolds,k=7)
-stack_rf(train=learners_train_1st,test=learners_test_1st,folds=pfolds,ntree=50)
-stack_svm(train=learners_train_1st,test=learners_test_1st,folds=pfolds,kernel)
-stack_xgboost(train=learners_train_1st,test=learners_test_1st,folds=pfolds,eta)
-stack_h2orf(train=learners_train_1st,test=learners_test_1st,folds=pfolds,ntrees)
-stack_h2odeep(train=learners_train_1st,test=learners_test_1st,folds=pfolds,activation=,initial_weight_distribution=,
+stack_knn(nfolds=nfolds-1,train=learners_train_1st,test=learners_test_1st,folds=pfolds,k=7)
+stack_rf(nfolds=nfolds-1,train=learners_train_1st,test=learners_test_1st,folds=pfolds,ntree=50)
+stack_svm(nfolds=nfolds-1,train=learners_train_1st,test=learners_test_1st,folds=pfolds,kernel)
+stack_xgboost(nfolds=nfolds-1,train=learners_train_1st,test=learners_test_1st,folds=pfolds,eta)
+stack_h2orf(nfolds=nfolds-1,train=learners_train_1st,test=learners_test_1st,folds=pfolds,ntrees)
+stack_h2odeep(nfolds=nfolds-1,train=learners_train_1st,test=learners_test_1st,folds=pfolds,activation=,initial_weight_distribution=,
               hidden=,hidden_dropout_ratios=,epochs=,rate=,l2=)
 
 construye(learners_train = learners_train, learners_test = learners_test )
 
 learners_train_2nd=learners_train
-learners_test_2nd=learner_test
+learners_test_2nd=learners_test
 modelos_2nd=modelos
 
 #######
 stacking=sapply(1:10,function(i){
   model=randomForest(Class~.,data=learners_train_2nd,ntree=50,type="prob")
   pred=predict(model,learners_test_2nd,type="prob")
-  return(auc(roc(pred[,2],test[,ncol(test)]),min=0,max = 1))
+  return(auc(roc(pred[,2],df[-indices[[1]],indice_clase]),min=0,max = 1))
 })
 
 nostacking=sapply(1:10,function(i){
   model=randomForest(Class~.,data=train,ntree=50,type="prob")
-  pred=predict(model,test[,-ncol(test)],type="prob")
-  return(auc(roc(pred[,2],test[,ncol(test)]),min=0,max = 1))
+  pred=predict(model,test,type="prob")
+  return(auc(roc(pred[,2],df[-indices[[1]],indice_clase]),min=0,max = 1))
 })
 
 print(mean(stacking))
